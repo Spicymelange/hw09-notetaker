b67bd84d5eb8405368b6c85e6bb1843254e4f059
Jordan Noe <jordanvinnoe@gmail.com>
2020-05-24 16:46:58 -0400

    apiRoute POST working, apiRoute delete working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,52 @@
-var router = require("express").Router();
-var fs = require("fs");
+const router = require("express").Router();
+const fs = require("fs");
+const { uuid } = require('uuidv4');
 
 router.get("/notes", function (req, res) {
-    fs.readFile("db/db.json", "utf8", function (err, data) {
-        if (err) throw err;
-        console.log(data);
-        res.send(JSON.parse(data));
-    });
-    console.log("expect the spanish inquis...");
-  
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+    console.log(data);
+    res.send(JSON.parse(data));
+  });
+  console.log("GET working");
 });
 
-// 
 router.post("/notes", function (req, res) {
-    res.json("false");
+  console.log(req.params);
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+
+   
+    let note = JSON.parse(data);
+    let newNote = req.body;
+    newNote.id = uuid();
+    note.push(newNote);
+
+    fs.writeFile("db/db.json", JSON.stringify(note), err => {
+      if (err) throw err;
+    });
+  });
+  res.json("POST responding");
 });
 
 router.delete("/notes/:id", function (req, res) {
-    res.send("test");
+  fs.readFile("db/db.json", "utf8", function(error, data) {
+          let noteId = req.params.id;
+          let noteData = JSON.parse(data);
+          noteData = noteData.filter(function(note) {
+              if (noteId != note.id) {
+                return true;
+              } else {
+                return false;
+              };
+          }); 
+    fs.writeFile("db/db.json", JSON.stringify(noteData), function (error) {
+      if (error)
+        throw error;
+      res.send(console.log("Deleted Successfully"));
+    });
+  });
 });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -16,24 +16,6 @@ app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-
-// Create New Characters - takes in JSON input
-// app.post("/api/characters", function (req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-//   var newCharacter = req.body;
-
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-//   console.log(newCharacter);
-
-//   characters.push(newCharacter);
-
-//   res.json(newCharacter);
-// });
-
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
