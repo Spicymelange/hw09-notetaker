d84537a531c1ff4726f28abbd658c75786cf139e
Jordan Noe <jordanvinnoe@gmail.com>
2020-05-24 17:37:15 -0400

    added execption handle, readme and gif

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hw09-notetaker
+
+>Keep track of your reminders and todo notes.  Save, review and delete your notes as needed.
+
+## Installation
+
+<code> npm install </code>
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const fs = require("fs");
-const { uuid } = require('uuidv4');
+const { uuid } = require("uuidv4");
 
 router.get("/notes", function (req, res) {
   fs.readFile("db/db.json", "utf8", function (err, data) {
@@ -16,13 +16,12 @@ router.post("/notes", function (req, res) {
   fs.readFile("db/db.json", "utf8", function (err, data) {
     if (err) throw err;
 
-   
     let note = JSON.parse(data);
     let newNote = req.body;
     newNote.id = uuid();
     note.push(newNote);
 
-    fs.writeFile("db/db.json", JSON.stringify(note), err => {
+    fs.writeFile("db/db.json", JSON.stringify(note), (err) => {
       if (err) throw err;
     });
   });
@@ -30,23 +29,22 @@ router.post("/notes", function (req, res) {
 });
 
 router.delete("/notes/:id", function (req, res) {
-  fs.readFile("db/db.json", "utf8", function(error, data) {
-          let noteId = req.params.id;
-          let noteData = JSON.parse(data);
-          noteData = noteData.filter(function(note) {
-              if (noteId != note.id) {
-                return true;
-              } else {
-                return false;
-              };
-          }); 
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    if (err) throw err;
+    let noteId = req.params.id;
+    let noteData = JSON.parse(data);
+    noteData = noteData.filter(function (note) {
+      if (noteId != note.id) {
+        return true;
+      } else {
+        return false;
+      }
+    });
     fs.writeFile("db/db.json", JSON.stringify(noteData), function (error) {
-      if (error)
-        throw error;
+      if (error) throw error;
       res.send(console.log("Deleted Successfully"));
     });
   });
 });
-
 
 module.exports = router;
